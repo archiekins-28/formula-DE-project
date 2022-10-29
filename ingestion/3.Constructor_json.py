4defdcb8cf1e8b6d8280b52ef096c0a1863e1e54
archiekins-28 <101446504+archiekins-28@users.noreply.github.com>
2022-10-29 19:27:06 +0530

    Incremental version of constructor files

--- ingestion/3.Constructor_json.py
+++ ingestion/3.Constructor_json.py
@@ -8,11 +8,21 @@
 
 # COMMAND ----------
 
+dbutils.widgets.text("p_data_source","")
+p_data_source=dbutils.widgets.get("p_data_source")
+
+# COMMAND ----------
+
+dbutils.widgets.text("p_file_date","2021-03-21")
+p_file_date=dbutils.widgets.get("p_file_date")
+
+# COMMAND ----------
+
 constructor_schema='constructorId INT,constructorRef STRING,name STRING,nationality STRING,url STRING'
 
 # COMMAND ----------
 
-costructor_df=spark.read.schema(constructor_schema).json(f'{raw_path}/constructors.json')
+costructor_df=spark.read.schema(constructor_schema).json(f'{raw_path}/inc-raw/{p_file_date}/constructors.json')
 
 
 # COMMAND ----------
@@ -32,13 +42,15 @@ costructor_dropped_df=costructor_df.drop('url')
 
 # COMMAND ----------
 
-from pyspark.sql.functions import current_timestamp
+from pyspark.sql.functions import current_timestamp,lit
 
 # COMMAND ----------
 
 final_constructor_df=costructor_dropped_df.withColumnRenamed('constructorId','constructor_id')\
 .withColumnRenamed('constructorRef','constructor_ref')\
-.withColumn('ingestion_date',current_timestamp())
+.withColumn('ingestion_date',current_timestamp())\
+.withColumn("data_source",lit(p_data_source))\
+.withColumn("file_date",lit(p_file_date))
 
 
 # COMMAND ----------
@@ -48,8 +60,21 @@ final_constructor_df=costructor_dropped_df.withColumnRenamed('constructorId','co
 
 # COMMAND ----------
 
-final_constructor_df.write.mode('overwrite').parquet(f'{processed_path}/processed/constructor')
+# final_constructor_df.write.mode('overwrite').parquet(f'{processed_path}/processed/constructor')
 
 # COMMAND ----------
 
-display(spark.read.parquet(f'{processed_path}/processed/constructor'))
+final_constructor_df.write.mode('overwrite').format('parquet').saveAsTable('f1_processsed.constructor')
+
+# COMMAND ----------
+
+display(spark.read.parquet(f'{processed_path}/constructor'))
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT * FROM f1_processsed.constructor
+
+# COMMAND ----------
+
+dbutils.notebook.exit('success')
